10428db7f46cd4888794d6a18bb5423f8440d3ba
Andreyna Marques Carvalho <andreyna.m.carvalho@gmail.com>
2024-05-15 09:42:12 -0300

    update aula 01

---
+++ Secao 01 - O que e Arrays/Aula 01 - O que e uma array.js
@@ -0,0 +1,12 @@
+// O QUE É UM ARRAY?
+
+// Um array é uma coleção ordenada de elementos que pode ser de qualquer
+// tipo (numeros, strings, objetos, etc.) usados para armazenar multiplos valores em uma variável
+
+const numbers = [1, 2, 3, 4, 5, 6, 7]
+const strings = ["De mamãe", "De Laurinha", "De escola"]
+const objects = [ {name: "Andreyna", age: 22 }, {name: "Naruto", age: 16 } ]
+
+console.log(`Numbers ${numbers[6]}`)
+console.log("strings " + strings[0])
+console.log(objects[0])
